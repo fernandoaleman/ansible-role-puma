11eead49f7c92f6d3a838d63c188408592901c59
Fernando Alemán <fernandoaleman@mac.com>
2025-05-28 15:15:14 -0500

    Ensure puma config directory

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,8 +1,13 @@
 # molecule/default/tests/test_default.py
+puma_config_dir = "/etc/puma"
+puma_user = "deploy"
 
 
-def test_host_is_reachable(host):
-    """Ensure etc directory exists"""
-    etc = host.file("/etc")
-    assert etc.exists
-    assert etc.is_directory
+def test_puma_config_dir_exists(host):
+    puma = host.file(puma_config_dir)
+    assert puma.exists, f"{puma_config_dir} does not existh"
+    assert puma.is_directory, f"{puma_config_dir} is not a directory"
+    assert puma.user == puma_user, f"{puma_config_dir} is not owned by {puma_user}"
+    assert puma.group == puma_user, f"{puma_config_dir} group is not {puma_user}"
+    assert puma.user == puma_user, f"{puma_config_dir} is not owned by {puma_user}"
+    assert puma.group == puma_user, f"{puma_config_dir} group is not {puma_user}"
